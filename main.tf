bbfe6071fc321a4fc3a839b41e5f92a2a36fc5d6
Vincent Pek <pjsiong@hotmail.com>
2020-11-11 21:11:42 +0800

    new interpolation test 2

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ variable "location" {
 
 
 resource "azurerm_virtual_network" "main" {
-  name                = "var.prefix-network"
+  name                = var.prefix-network
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group
@@ -42,13 +42,13 @@ resource "azurerm_virtual_network" "main" {
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = "var.resource_group"
+  resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "var.prefix-nic"
+  name                = var.prefix-nic
   location            = var.location
   resource_group_name = var.resource_group
 
@@ -60,7 +60,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "var.prefix-vm"
+  name                  = var.prefix-vm
   location            = var.location
   resource_group_name = var.resource_group
   network_interface_ids = [azurerm_network_interface.main.id]
