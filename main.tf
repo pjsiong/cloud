03d61fb7dc2486b63258b01b385f27ba3216c54a
Vincent Pek <pjsiong@hotmail.com>
2020-11-11 20:47:33 +0800

    virtual machine test

--- main.tf
+++ main.tf
@@ -19,12 +19,80 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg1" {
-  name     = "AZU-R-Sandbox_Terraform-remote"
-  location = "eastasia"
 
+variable "prefix" {
+  default = "AZU-R-Sandbox_"
+}
+
+variable "resource_group" {
+  default = "AZU-R-Sandbox_Terraform"
+}
+
+variable "location" {
+  default = "eastasia"
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${var.resource_group}"
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
   tags = {
-    description = "Resource group created in sandbox with remote backend"
-    Team        = "Testing"
+    environment = "staging"
   }
 }
