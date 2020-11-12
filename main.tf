d9d255c6ef1b445da641382437a06a6bbd459042
Vincent Pek <pjsiong@hotmail.com>
2020-11-12 16:27:08 +0800

    terraform cloud variable testing 3

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ terraform {
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   #cls_terraform_testing
-  subscription_id = "5387925a-1073-4218-badd-44b79b144511"
-  client_id       = "23273d00-8589-4600-af59-2040ae7a9caa"
-  client_secret   = "7pf5wgcdZG.Nm~luEK.ReyM.~47XHmdm-8"
-  #tenant_id       = "545b5243-ef8b-49a7-ad4e-77d48c0f2abb"
+
   tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 
   features {}
 }
@@ -34,6 +34,15 @@ variable "location" {
 }
 
 variable "tenant_id" {
+  default = ""
+}
+variable "subscription_id" {
+  default = ""
+}
+variable "client_id" {
+  default = ""
+}
+variable "client_secret" {
   default = ""
 }
 
