3453475d79873939fe090164951262f08e3e6bb9
Vincent Pek <pjsiong@hotmail.com>
2020-11-19 11:08:44 +0800

    test local variable

--- main.tf
+++ main.tf
@@ -20,10 +20,14 @@ provider "azurerm" {
   features {}
 }
 
-
-variable "prefix" {
-  default = "AZU-R-Sandbox_"
+locals {
+  prefix = "AZU-R-Sandbox_"
 }
+
+
+#variable "prefix" {
+#  default = "AZU-R-Sandbox_"
+#}
 
 variable "resource_group" {
   default = "AZU-R-Sandbox_Terraform"
@@ -75,40 +79,3 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location            = var.location
-  resource_group_name = var.resource_group
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-}
